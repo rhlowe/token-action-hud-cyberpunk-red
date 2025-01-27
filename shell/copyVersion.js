f0d8b150a485b2786d9763523b9a20935fbc9b87
Rob Lowe <rhlowe@gmail.com>
2025-01-27 12:08:47 -0600

    set up versioning workflow

---
+++ shell/copyVersion.js
@@ -0,0 +1,8 @@
+const {version} = require('../package.json');
+const shell = require('shelljs');
+// const next_version = shell.env['npm_package_version'];
+
+shell.echo(`Applying ${version} to module.json in ${shell.pwd()}`);
+shell.rm('-f', 'module.json');
+shell.cp('-f', 'module_raw.json', `module.json`);
+shell.sed('-i', 'VERSION_TO_REPLACE', version, 'module.json');
