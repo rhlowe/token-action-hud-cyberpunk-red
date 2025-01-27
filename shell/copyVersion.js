486312d27180a04ba500b5a3c5471f38989601e9
Rob Lowe <rhlowe@gmail.com>
2025-01-27 13:25:12 -0600

    1.0.0-beta.15

--- shell/copyVersion.js
+++ shell/copyVersion.js
@@ -6,3 +6,5 @@ shell.echo(`Applying ${version} to module.json in ${shell.pwd()}`);
 shell.rm('-f', 'module.json');
 shell.cp('-f', 'module_raw.json', `module.json`);
 shell.sed('-i', 'VERSION_TO_REPLACE', version, 'module.json');
+shell.exec('git add .');
+shell.exec('git commit --amend --no-edit');
